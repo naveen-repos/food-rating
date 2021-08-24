2df76a298963c48e5ef71744aed0457805bc10f7
Naveen <naveen.kandula@tilicho.in>
2021-08-24 22:57:27 +0530

    added parser for the get session api

--- lambda/src/controllers/session/get-sessions/sanitizer.js
+++ lambda/src/controllers/session/get-sessions/sanitizer.js
@@ -1,4 +1,12 @@
-const { identity } = require('ramda');
-
-const sanitizer = identity;
+const { applySpec, pathOr, pipe, map, length } = require('ramda');
+const sessionCard = applySpec({
+  sessionId: pathOr('', ['session', 'id']),
+  sessionName: pathOr('', ['session', 'name']),
+  sessionStatus: pathOr('', ['session', 'status']),
+  menuId: pathOr('', ['menu', 'id']),
+  items: pathOr([], ['menu', 'items']),
+  itemsCount: pipe(pathOr([], ['menu', 'items']), length),
+  overAllRating: pathOr(0, ['overAllRating']),
+});
+const sanitizer = map(sessionCard);
 module.exports = { sanitizer };
