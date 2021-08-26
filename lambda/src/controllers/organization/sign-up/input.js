3ea042f16fbb672583f4d51e47ec97b13a01a929
Naveen <naveen.kandula@tilicho.in>
2021-08-26 06:10:30 +0530

    fixing the singup issue

--- lambda/src/controllers/organization/sign-up/input.js
+++ lambda/src/controllers/organization/sign-up/input.js
@@ -4,6 +4,5 @@ const inputValidator = Joi.object({
   adminName: Joi.string().required(),
   type: Joi.string().required().valid('organization', 'hotel'),
   email: Joi.string().email().required(),
-  organizationId: Joi.string().required(),
 });
 module.exports = { inputValidator };
