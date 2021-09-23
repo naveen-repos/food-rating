d696b938a0e80064aa0bfbc1e015a52b9c626701
Naveen <naveen.kandula@tilicho.in>
2021-09-23 11:12:35 +0530

    prasing incoming event and pinting

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -3,12 +3,31 @@ const awsServerlessExpress = !isTestingEnvironment
   ? require('aws-serverless-express')
   : require('./server');
 const app = require('./server');
+const { pickAll } = require('./ramda');
 
 const binaryMimeTypes = [];
 const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
 
+const logEvent = (event) => {
+  try {
+    const parsedEvent = pickAll(
+      [
+        'path',
+        'httpMethod',
+        'pathParameters',
+        'pathParameters',
+        'body',
+        'isBase64Encoded',
+      ],
+      event
+    );
+    console.log({ parsedEvent, authToken: event.headers.Authorization });
+  } catch (ex) {
+    console.error(ex);
+    console.log({ event });
+  }
+};
 exports.handler = (event, context) => {
-  console.log(event);
-
+  logEvent(event);
   return awsServerlessExpress.proxy(server, event, context);
 };
